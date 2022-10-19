33ce744548c6a3cf6ce6206f8d856ba1ff42679d
desfint <81056093+desfint@users.noreply.github.com>
2022-10-19 19:43:55 -0400

    Functional Caesar Cipher

---
+++ main.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(str, num)
+    alphabet = ("a".."z").to_a
+
+    str.split("").map do |char|
+        capitalized = (char == char.upcase)
+        char = char.downcase
+        if alphabet.include?(char)
+            char = char.downcase
+            char = alphabet[(alphabet.index(char) + num) % alphabet.length]
+            
+            capitalized ? char.upcase : char
+        else
+            char
+        end
+    end.join("")
+end
